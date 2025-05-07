29aafc318f743c3fc9a21617f7307eef0e2ec7eb
bdelate <brendon.d@tangentsolutions.co.za>
2025-05-07 17:12:23 +0200

    lesson done

--- src/api/endpoints/barks.py
+++ src/api/endpoints/barks.py
@@ -1,4 +1,5 @@
 from ninja import Router
+from api.schemas.bark_schemas import BarkSchemaOut
 
 router = Router()
 
@@ -11,13 +12,13 @@ def barks_list(request):
     return [{'id':1, 'message': 'bark 1!'}, {'id':2, 'message': 'bark 2!'}, {'id':3, 'message': 'bark 3!'}]
 
 
-@router.get("/{bark_id}/", response={200: dict, 404: dict})
+@router.get("/{bark_id}/", response={200: BarkSchemaOut, 404: dict})
 def get_bark(request, bark_id: int):
     """
     Bark detail endpoint that returns a single bark.
     """
     if bark_id in [1, 2, 3]:
-        return 200, {"id": bark_id, "message": f"bark {bark_id}!"}
+        return 200, {"id": bark_id, "message": f"bark {bark_id}!", "breed": "Poodle"}
     return 404, {"error": "Bark not found"}
 
 
