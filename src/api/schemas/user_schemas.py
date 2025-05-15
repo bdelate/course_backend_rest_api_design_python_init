0b9ac7e03d5811321d44484e9865d04a7818c993
bdelate <brendon.d@tangentsolutions.co.za>
2025-05-15 10:42:51 +0200

    done

--- src/api/schemas/user_schemas.py
+++ src/api/schemas/user_schemas.py
@@ -1,5 +1,6 @@
 from ninja import ModelSchema
 from core.models import DogUserModel
+from pydantic import field_validator
 
 class DogUserSchemaOut(ModelSchema):
     """Schema for dog user responses"""
@@ -15,3 +16,10 @@ class DogUserCreateSchemaIn(ModelSchema):
     class Meta:
         model = DogUserModel
         fields = ['username']
+
+    @field_validator('username')
+    @classmethod
+    def validate_username_min_length(cls, v: str) -> str:
+        """Ensure is at least 3 characters long"""
+        if len(v) < 3:
+            raise ValueError("Username must be at least 3 characters long")
