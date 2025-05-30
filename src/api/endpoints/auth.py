dbff927d4490f6a67fd5c305d3f2b449a8bcc657
bdelate <brendon.d@tangentsolutions.co.za>
2025-05-30 08:33:41 +0200

    done

--- src/api/endpoints/auth.py
+++ src/api/endpoints/auth.py
@@ -3,6 +3,8 @@ from api.schemas.common_schemas import ErrorSchemaOut
 from api.schemas.auth_schemas import TokenRequestSchemaOut, TokenRequestSchemaIn
 from core.models import AuthTokenModel
 from django.contrib.auth import authenticate
+from django.utils import timezone
+
 router = Router()
 
 
@@ -16,10 +18,17 @@ def get_token(request, credentials: TokenRequestSchemaIn):
         token_request: The token request schema containing username and password
     
     Returns:
-        A token if authentication is successful, or an error if not.
+        A access and refresh token if authentication is successful,
     """
     user = authenticate(username=credentials.username, password=credentials.password)
     if user is None:
         return 401, {"error": "Invalid credentials"}
-    token, _ = AuthTokenModel.objects.get_or_create(user=user)
-    return 200, {"token": token.key}
+    AuthTokenModel.objects.filter(user=user).delete()
+    access = AuthTokenModel.objects.create(user=user, token_type=AuthTokenModel.TOKEN_TYPE_ACCESS)
+    refresh = AuthTokenModel.objects.create(user=user, token_type=AuthTokenModel.TOKEN_TYPE_REFRESH)
+    expires_in = int((access.expires - timezone.now()).total_seconds())
+    return 200, {
+        "access_token": access.key,
+        "refresh_token": refresh.key,
+        "expires_in": expires_in
+    }
--- src/api/schemas/auth_schemas.py
+++ src/api/schemas/auth_schemas.py
@@ -2,14 +2,20 @@ from ninja import Schema
 
 
 class TokenRequestSchemaIn(Schema):
-    """
-    Schema for token request input.
-    """
+    """Schema for token request with username and password"""
+
     username: str
     password: str
 
+
 class TokenRequestSchemaOut(Schema):
-    """
-    Schema for token request output.
-    """
-    token: str
+    """Schema for token response"""
+
+    access_token: str
+    refresh_token: str
+    expires_in: int
+
+
+class RefreshTokenRequestSchemaIn(Schema):
+    """Schema for refresh token request"""
+    refresh_token: str
